e3bb3275ddc6aabcef552817d6106b509006385a
malaka <malaka.14@cse.mrt.ac.lk>
2017-09-02 17:12:46 +0530

    remove angular material

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,14 +3,12 @@ import {NgModule} from "@angular/core";
 
 import {AppComponent} from "./app.component";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {AngularMaterialModule} from "./app-styles/angular-material.module";
-import {PluginsModule} from "./app-styles/plugins.moudles";
 import {RouterModule} from "@angular/router";
 import {MainLayoutComponent} from "./app-layout/main-layout/main-layout.component";
 import {SideNavbarComponent} from "./app-widgets/side-navbar/side-navbar.component";
 import {MainContainerComponent} from "./app-widgets/main-container/main-container.component";
 import {LoginPageComponent} from "./app-widgets/login-page/login-page.component";
-import {APP_ROUTES} from "./app-routes/app-route.module";
+import {APP_ROUTES} from "./route.module";
 import {CreateAccountPageComponent} from "./app-widgets/create-account-page/create-account-page.component";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {NgbModule, NgbProgressbarModule, NgbTabsetModule} from "@ng-bootstrap/ng-bootstrap";
@@ -31,8 +29,6 @@ import {SocialTestComponent} from "./app-widgets/social-test/social-test.compone
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    PluginsModule,
-    AngularMaterialModule,
     RouterModule.forRoot(APP_ROUTES, { enableTracing: true }),
     FormsModule,
     ReactiveFormsModule,
